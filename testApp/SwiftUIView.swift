35dc675ec05bfdd1358c4d0b8e239e9249e86698
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2024-10-26 14:10:22 +0300

    Test commit

---
+++ testApp/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  testApp
+//
+//  Created by Vladislav Miroshnichenko on 26.10.2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Vladislav Miroshnichenko on 26.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
