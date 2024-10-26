4bed0831215e416fd9f8f8aa77c4c1c6e7d57d9c
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2024-10-26 14:39:49 +0300

    Test 6

---
+++ testApp/sdas.swift
@@ -0,0 +1,18 @@
+//
+//  sdas.swift
+//  testApp
+//
+//  Created by Vladislav Miroshnichenko on 26.10.2024.
+//
+
+import SwiftUI
+
+struct sdas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sdas()
+}
