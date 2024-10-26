ee331af5799b788649120a2cf5921d63d13ed53e
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2024-10-26 14:44:09 +0300

    test 8

---
+++ testApp/dasdasddadasdasd.swift
@@ -0,0 +1,19 @@
+
+//
+//  dasdasddadasdasd.swift
+//  testApp
+//
+//  Created by Vladislav Miroshnichenko on 26.10.2024.
+//
+
+import SwiftUI
+
+struct dasdasddadasdasd: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dasdasddadasdasd()
+}
