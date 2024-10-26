0a05ea705da2a959bbbfbade96b7bb05c4bd6a01
Vladislav Miroshnichenko <vladislavmiro1@gmail.com>
2024-10-26 14:47:42 +0300

    test 11

---
+++ testApp/test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  testApp
+//
+//  Created by Vladislav Miroshnichenko on 26.10.2024.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
